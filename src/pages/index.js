e0e4f8127c8460ad85af53e1f36deb641d285ebd
Gmagxcx <magooo.xyz@gmail.com>
2024-11-21 17:09:21 +0800

    Added Analytics
    Added code for analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import DummyPage from '@/components/pages/dashboard';
 import Footer from './Footer';
 import Header from './Header';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function HomePage() {
   return (
     <div>
       <Header />
       <DummyPage title="Home Page" />
+      <Analytics />
       <Footer />
 
 
