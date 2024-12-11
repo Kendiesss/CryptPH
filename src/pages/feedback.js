f5c3b3ca42a87616126e9e241bf9c886a60c8be0
Kennn <63950642+Kendiesss@users.noreply.github.com>
2024-12-11 23:34:21 +0800

    Add files via upload

---
+++ src/pages/feedback.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DummyPage from '@/components/pages/feedback'
+import Footer from './Footer';
+import Header from './Header2';
+
+export default function feedback() {
+    return (
+        <div>
+          <DummyPage title="feedback" />
+        </div>
+      );
+    }
